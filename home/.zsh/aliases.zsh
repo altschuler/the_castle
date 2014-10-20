a021d28ba2d9b5fff1969d55ef7af79643b064f3
Simon Altschuler <simon@altschuler.dk>
2014-10-20 22:59:21 +0200

    write one do another

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -8,8 +8,11 @@ alias pm='sudo pacman'
 
 # git
 alias gg='git status'
+alias gd='git diff'
+alias gdc='git diff --cached'
 
 # misc
+alias suspend='sudo systemctl suspend'
 alias ww='sudo wifi-menu'
 alias p='evince'
 alias startmysql="sudo /usr/bin/mysqld_safe --datadir=/var/lib/mysql"
