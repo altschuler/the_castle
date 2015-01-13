8c27153e96453a15eee17496d400e52c160f41b6
Simon Altschuler <simon@altschuler.dk>
2015-01-13 16:02:32 +0100

    listen up

--- home/.zsh/functions.zsh
+++ home/.zsh/functions.zsh
@@ -22,3 +22,44 @@ sc () {
         xrandr --output $id --auto --$pos eDP1
     fi
 }
+
+# radio!
+ro () {
+    case "$1" in
+        p1)
+            echo "Playing: DR P1"
+            vlc --intf dummy http://live-icy.gss.dr.dk:/A/A03L.mp3.m3u
+            ;;
+        p2)
+            echo "Playing: DR P2"
+            vlc --intf dummy http://live-icy.gss.dr.dk:/A/A04L.mp3.m3u
+            ;;
+        p3)
+            echo "Playing: DR P3"
+            vlc --intf dummy http://live-icy.gss.dr.dk:/A/A05L.mp3.m3u
+            ;;
+        p5)
+            echo "Playing: DR P5"
+            vlc --intf dummy http://live-icy.gss.dr.dk:/A/A25L.mp3.m3u
+            ;;
+        p6)
+            echo "Playing: DR P6 BEAT"
+            vlc --intf dummy http://live-icy.gss.dr.dk:/A/A29L.mp3.m3u
+            ;;
+        p7)
+            echo "Playing: DR P7 MIX"
+            vlc --intf dummy http://live-icy.gss.dr.dk:/A/A21L.mp3.m3u
+            ;;
+        p8)
+            echo "Playing: DR P8 JAZZ"
+            vlc --intf dummy http://live-icy.gss.dr.dk:/A/A22L.mp3.m3u
+            ;;
+        nyheder)
+            echo "Playing: DR Nyheder"
+            vlc --intf dummy http://live-icy.gss.dr.dk:/A/A02L.mp3.m3u
+            ;;
+        *)
+            echo "Unknown channel '$1'"
+            ;;
+    esac
+}
